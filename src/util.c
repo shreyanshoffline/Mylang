5ba1b174f9aa3b0e7bc35f74fa875dec4e9d777c
ghostypeeps148 <wijeratnek12@gmail.com>
2026-03-09 03:03:41 -0400

    remove comment and fix a test

--- src/util.c
+++ src/util.c
@@ -5,7 +5,6 @@ char* trim_leading_spaces(char* str) {
         i++;
     }
     const int newlen = strlen(str) - i;
-    // Something here causes the next byte to get corrupted. I have no idea why. Someone plz fix.
     char dest[newlen+1];
     strncpy(dest, str + i, newlen);
     dest[newlen] ='\0';
